14bf14c7c8ccbb643c6b4a020ab9097157ba272a
dengqilong <554611551@qq.com>
2020-03-26 10:25:02 +0800

    demo

---
+++ src/main/java/com/dql/shiro/config/ShiroConfig.java
@@ -0,0 +1,10 @@
+package com.dql.shiro.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ShiroConfig {
+
+    
+
+}
---
+++ src/main/java/com/dql/shiro/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.dql.shiro.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/login")
+    public String login(HttpServletRequest request, HttpServletResponse response, Model model) {
+        return "index";
+    }
+
+}
